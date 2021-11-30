2c417d969e331a4cdecefb240a354bcea427671b
Sharon Gómez <sharonrubi98@gmail.com>
2021-11-29 23:35:56 -0600

    html base y js base para modulo de inventario
    modulo de inventario, se agrego maquetación con html y js base

---
+++ Primer aplicación con JavaScript/index.js
@@ -0,0 +1,106 @@
+const form = document.getElementsByTagName("form")[0];
+/** @type {HTMLInputElement} */
+const inputCodigo = document.getElementById("codigo");
+/** @type {HTMLInputElement} */
+const inputNombre = document.getElementById("nombre");
+/** @type {HTMLInputElement} */
+const inputCantidad = document.getElementById("cantidad");
+/** @type {HTMLInputElement} */
+const inputPrecio = document.getElementById("precio");
+/** @type {HTMLInputElement} */
+const selectCategoria = document.getElementById("categoria");
+const tbody = document.getElementsByTagName("tbody")[0];
+const cantidadTotalElement = document.getElementById("cantidad-total");
+const precioTotalElement = document.getElementById("precio-total");
+const granTotalElement = document.getElementById("gran-total");
+let indice = 1;
+let cantidadTotal = 0;
+let precioTotal = 0;
+let granTotal = 0;
+let currentRow;
+form.addEventListener("submit", onSubmit);
+
+/**
+ * 
+ * @param {Event} event 
+ */
+function onSubmit(event) {
+  event.preventDefault();
+
+  const data = new FormData(form);
+  const values = Array.from(data.entries());
+  const [frmCodigo, frmNombre, frmCantidad, frmPrecio, frmCategoria] = values;
+  let codigo = frmCodigo[1];
+  const nombre = frmNombre[1];
+  const cantidad = frmCantidad[1];
+  const precio = frmPrecio[1];
+  const categoria = frmCategoria[1];
+  const total = cantidad * precio;
+
+  cantidadTotal = parseFloat(cantidad) + cantidadTotal;
+  precioTotal = parseFloat(precio) + precioTotal;
+  granTotal = parseFloat(total) + granTotal;
+
+  let tr;
+  if (!codigo) {
+    codigo = indice++;
+    tr = document.createElement("tr");
+    tbody.appendChild(tr);
+  } else {
+    tr = currentRow;
+  }
+  tr.dataset.categoria = categoria;
+  tr.innerHTML = `
+    <td>${codigo}</td>
+    <td>${nombre}</td>
+    <td>${cantidad}</td>
+    <td>${precio}</td>
+    <td>${total}</td>
+    <td><a href='#' onClick='onEdit(event)'>Editar</a> | <a href='#' onClick='onDelete(event)'>Eliminar</a></td>
+  `;
+ 
+
+  cantidadTotalElement.innerText = cantidadTotal;
+  precioTotalElement.innerText = precioTotal;
+  granTotalElement.innerText = granTotal;
+
+  form.reset();
+  //posicionar el curso en el primer input ejercicio 
+}
+
+/**
+ * 
+ * @param {Event} event 
+ */
+function onEdit(event) {
+  event.preventDefault();
+  /** @type {HTMLElement} */
+  const anchor = event.target;
+  const tr = anchor.parentElement.parentElement;
+  const celdas = tr.getElementsByTagName("td");
+  const [tdCodigo, tdNombre, tdCantidad, tdPrecio] = celdas;
+  console.log("tdCantidad", tdCantidad.innerText);
+
+  inputCodigo.value = tdCodigo.innerText;
+  inputNombre.value = tdNombre.innerText;
+  inputCantidad.value = tdCantidad.innerText;
+  inputPrecio.value = tdPrecio.innerText;
+  selectCategoria.value = tr.dataset.categoria;
+
+  currentRow = tr;
+
+}
+
+/**
+ * 
+ * @param {Event} event 
+ */
+ function onDelete(event) {
+  event.preventDefault();
+
+  /** @type {HTMLElement} */
+  const anchor = event.target;
+  const tr = anchor.parentElement.parentElement;
+  tbody.removeChild(tr);
+
+}
