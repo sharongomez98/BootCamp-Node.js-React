54679955c40d08c887c7c81b8f1f29903ff1e93b
Sharon Gómez <sharonrubi98@gmail.com>
2022-01-07 04:32:21 -0600

    configuracion basica de wepack y react.

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,10 @@
-// import "./js/app";
 import React from "react";
 import ReactDom from "react-dom";
-import "./index.css"
-import logo from "./logo.png"
 
 const container = document.getElementById("root");
 const App = () => (
-    <div className="app">
-        <h1>Hola React !</h1>
-        <img src={logo} />
+    <div>
+        <h1>Hola React</h1>
     </div>
 );
 
