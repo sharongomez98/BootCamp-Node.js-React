48e2f3005c86067c1d30e94a2104d40568c1df5a
Sharon Gómez <sharonrubi98@gmail.com>
2022-01-11 03:37:55 -0600

    request http con axios

--- client/src/index.js
+++ client/src/index.js
@@ -2,9 +2,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
+import axios from "axios";
 import App from "./app.js";
 import store from "./store";
-
 
 const rootElement = document.getElementById("root");
 
@@ -14,3 +14,15 @@ ReactDOM.render(
   </Provider>,
   rootElement
 );
+test();
+
+async function test() {
+  try {
+    console.log("Antes del fetch");
+    const response = await axios.get("http://localhost:5001/producto");
+    const productos = response.data;
+    console.log("productos: ", productos);
+  } catch (error) {
+    console.error("error en el request: ", error);
+  }
+}
